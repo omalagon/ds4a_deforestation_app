0765cf81a5e054036f5286a507f84b9a9a62cae9
Oscar Malagon <oscar.malagon@kinesso.com>
2020-07-24 12:04:44 -0500

    Adding s3 support

--- apps/RawData/Raw_data_panel_callback.py
+++ apps/RawData/Raw_data_panel_callback.py
@@ -7,60 +7,99 @@ import pandas as pd
 import json
 import plotly.express as px
 import dash_core_components as dcc
+import boto3
+import io
+
+s3 = boto3.client(service_name='s3', region_name="sa-east-1")
+base_s3_url = "https://deforestation-app.s3-sa-east-1.amazonaws.com/"
+
+file_dict = {
+    'raw_dane_es_po':
+        {'file_key': "Dane/anexo-Estimaciones_de_Poblacion_2005-2017-Municipal.csv",
+         'sep': '\t',
+         'encoding': 'UTF-8',
+         'title': 'Estimaciones_de_Poblacion_2005-2017-Municipal'},
+    'raw_dane_cnd_2016_2018':
+        {'file_key': "Dane/2018-provisional-valor-agregado-municipio.txt",
+         'sep': '\t',
+         'encoding': 'UTF-8',
+         'title': 'Cuentas nacionales departamentales consolidado 2016-2018'},
+    'raw_dane_cnd_2016':
+        {'file_key': "Dane/2018-provisional-valor-agregado-municipio_2016.txt",
+         'sep': '\t',
+         'encoding': 'UTF-8',
+         'title': 'Cuentas nacionales departamentales 2016'},
+    'raw_dane_cnd_2017':
+        {'file_key': "Dane/2018-provisional-valor-agregado-municipio_2017.txt",
+         'sep': '\t',
+         'encoding': 'UTF-8',
+         'title': 'Cuentas nacionales departamentales 2017'},
+    'raw_dane_cnd_2018':
+        {'file_key': "Dane/2018-provisional-valor-agregado-municipio_2018.txt",
+         'sep': '\t',
+         'encoding': 'UTF-8',
+         'title': 'Cuentas nacionales departamentales 2018'},
+    'raw_CMH_AS_1981_2012':
+        {'file_key': "CentroMemoriaHistorico/AsesinatosSelectivos1981-2012.csv",
+         'sep': ',',
+         'encoding': 'ISO-8859-1',
+         'title': 'Asesinatos en colombia 1981 - 2012'},
+    'raw_CMH_SC_1970_2010':
+        {'file_key': "CentroMemoriaHistorico/SecuestrosColombia1970-2010.csv",
+         'sep': ',',
+         'encoding': 'ISO-8859-1',
+         'title': 'Secuestros en colombia 1970 - 2010'}
+}
 
 
 def register_callback(app):
     @app.callback(Output('raw_content', 'children'),
-              [
-                  Input('raw_geo', 'n_clicks'),
-                  Input('raw_dane_es_po', 'n_clicks'),
-                  Input('raw_dane_cnd_2016_2018', 'n_clicks'),
-                  Input('raw_dane_cnd_2016', 'n_clicks'),
-                  Input('raw_dane_cnd_2017', 'n_clicks'),
-                  Input('raw_dane_cnd_2018', 'n_clicks'),
-                  Input('raw_CMH_AS_1981_2012', 'n_clicks'),
-                  Input('raw_CMH_SC_1970_2010', 'n_clicks'),
-              ])
+                  [
+                      Input('raw_geo', 'n_clicks'),
+                      Input('raw_dane_es_po', 'n_clicks'),
+                      Input('raw_dane_cnd_2016_2018', 'n_clicks'),
+                      Input('raw_dane_cnd_2016', 'n_clicks'),
+                      Input('raw_dane_cnd_2017', 'n_clicks'),
+                      Input('raw_dane_cnd_2018', 'n_clicks'),
+                      Input('raw_CMH_AS_1981_2012', 'n_clicks'),
+                      Input('raw_CMH_SC_1970_2010', 'n_clicks'),
+                  ])
     def display(raw_geo, raw_dane_es_po, raw_dane_cnd_2016_2018, raw_dane_cnd_2016, raw_dane_cnd_2017,
                 raw_dane_cnd_2018, raw_CMH_AS_1981_2012, raw_CMH_SC_1970_2010):
-        print('->');
+        print('->')
         ctx = dash.callback_context
         if ctx.triggered:
             caso = ctx.triggered[0]['prop_id'].split('.')[0]
-            if (caso == 'raw_geo'):
+            if caso == 'raw_geo':
                 return dcc.Graph(figure=mapa)
-            if (caso == 'raw_dane_es_po'):
-                return CreateTableFromFile("data/Dane/anexo-Estimaciones_de_Poblacion_2005-2017-Municipal.csv", '\t',
-                                           'anexo-Estimaciones_de_Poblacion_2005-2017-Municipal');
-            if (caso == 'raw_dane_cnd_2016_2018'):
-                return CreateTableFromFile("data/Dane/2018-provisional-valor-agregado-municipio.txt", '\t',
-                                           'Cuentas nacionales departamentales consolidado 2016-2018');
-            if (caso == 'raw_dane_cnd_2016'):
-                return CreateTableFromFile("data/Dane/2018-provisional-valor-agregado-municipio_2016.txt", '\t',
-                                           'Cuentas nacionales departamentales 2016');
-            if (caso == 'raw_dane_cnd_2017'):
-                return CreateTableFromFile("data/Dane/2018-provisional-valor-agregado-municipio_2017.txt", '\t',
-                                           'Cuentas nacionales departamentales 2017');
-            if (caso == 'raw_dane_cnd_2018'):
-                return CreateTableFromFile("data/Dane/2018-provisional-valor-agregado-municipio_2018.txt", '\t',
-                                           'Cuentas nacionales departamentales 2018');
-            if (caso == 'raw_CMH_AS_1981_2012'):
-                return CreateTableFromFile("data/CentroMemoriaHistorico/AsesinatosSelectivos1981-2012.csv", ',',
-                                           'Asesinatos en colombia 1981 - 2012');
-            if (caso == 'raw_CMH_SC_1970_2010'):
-                return CreateTableFromFile("data/CentroMemoriaHistorico/SecuestrosColombia1970-2010.csv", ',',
-                                           'Secuestros en colombia 1970 - 2010');
+
+            elif caso in file_dict:
+                found_file = file_dict.get(caso)
+                return create_table_from_file(found_file['file_key'],
+                                              found_file['sep'],
+                                              found_file['encoding'],
+                                              found_file['title'])
+
             else:
                 return 'No implementado: ' + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S");
         return ''
 
-    def CreateTableFromFile(file, demitador, titulo):
-        print('Inicia lectura de: ' + file);
-        df = pd.read_csv(file, delimiter=demitador, decimal=".", encoding='ISO-8859-1', skiprows=0, nrows=20);
-        print(df.describe());
+    def create_table_from_file(file, demitador, encoding, titulo):
+        print('Inicia lectura de: ' + file)
+        obj = s3.get_object(Bucket='deforestation-app', Key=file)
+
+        df = pd.read_csv(io.BytesIO(obj['Body'].read()),
+                         delimiter=demitador,
+                         decimal=".",
+                         encoding=encoding,
+                         skiprows=0,
+                         nrows=20)
+
         return html.Div(
             [
                 html.H1(titulo),
+                html.A("Descargar..", href=f"{base_s3_url}{file}", target="_blank"),
+                html.Br(),
                 html.Div([
                     dash_table.DataTable(
                         id='table',
