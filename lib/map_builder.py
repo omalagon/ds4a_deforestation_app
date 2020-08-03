b22c8a600d510fd4733d39d7340de519ead53a59
Oscar Malagon <oscar.malagon@kinesso.com>
2020-08-02 20:25:50 -0500

    new color scale

--- lib/map_builder.py
+++ lib/map_builder.py
@@ -1,5 +1,18 @@
 import plotly.express as px
-import os
+
+color_scale = [
+    "rgb(0,104,55)",
+    "rgb(26,152,80)",
+    "rgb(102,189,99)",
+    "rgb(166,217,106)",
+    "rgb(217,239,139)",
+    "rgb(255,255,191)",
+    "rgb(254,224,139)",
+    "rgb(253,174,97)",
+    "rgb(244,109,67)",
+    "rgb(215,48,39)",
+    "rgb(165,0,38)",
+]
 
 
 def build_map_figure(data, geojson, indicator):
@@ -12,8 +25,9 @@ def build_map_figure(data, geojson, indicator):
                                 mapbox_style="carto-positron",
                                 # Mapbox style, for different maps you need a Mapbox account and a token
                                 center={"lat": 7.5, "lon": -75.133},  # Center
-                                color_continuous_scale=os.environ.get('MAP_COLOR_SCALE'),  # Color Scheme
+                                color_continuous_scale=color_scale,  # Color Scheme
                                 opacity=0.5,  # Opacity of the map
                                 width=900,
                                 height=800
+
                                 )
